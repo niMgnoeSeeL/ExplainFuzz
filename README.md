6f8912b4d9f00373c743eace5af89656beff57cf
Annaelle <annaellebaiget@g.ucla.edu>
2025-05-09 16:23:23 -0700

    start setting up the generator as a black box

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Grammar-test-input-generation
+
+## Grammarinator_fuzzing
+
+In order to run it as a package, run this :
+
+```
+pip install -e grammarinator_fuzzing/
+```
+
+Create an `.env` file by adding `ANTLR_JAR_PATH=`
+
+## custom_generator_sql
+
+In order to run it as a package, run this :
+
+```
+pip install -e custom_generator_sql/
+```
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from grammarinator_fuzzing.main import main as grammar_fuzz_main
+from custom_generator_sql.generator import main as custom_gen_sql_main
 
 def fuzzing_campaign(prefix_grammar,domain,start_rule):
     grammar_fuzz_main(
@@ -8,3 +9,8 @@ def fuzzing_campaign(prefix_grammar,domain,start_rule):
         num_inputs=10,
         first_time=False
     )
+
+def concretization(conninfo,anonymized_queries,lexer_file_path):
+    max_queries=1000
+    length_batch=20
+    custom_gen_sql_main(conninfo,anonymized_queries,max_queries,length_batch,lexer_file_path,dry_run=False)
