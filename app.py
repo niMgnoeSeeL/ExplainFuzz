6aa57da2729000ff04c8b3d9f6f3a5c318942b53
Annaelle <annaellebaiget@g.ucla.edu>
2025-05-28 12:16:59 -0700

    add position paramter to queries + fix small mistaked in inference file

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from pathlib import Path
 import gradio as gr
-import zipfile
-import os
 
 from inference import ask_query, load_model_and_info
 from main import get_literal_token_mapping, main_generate_inputs
@@ -30,17 +28,29 @@ def compute_probability(domain, query_type, inputs, mode):
     return ask_query(model, lit_map, lit_size, max_length, query_type, inputs)
 
 
-def dispatch_probability_function(domain, question, lit1, lit2, mode):
+def dispatch_probability_function(domain, question, lit1, lit2, lit3, pos, mode):
     fn = question_to_function[question]
     if not fn:
         return "Unsupported question selected."
 
     count = literal_count_by_question[question]
+    has_pos = position_mapping[question]
     try:
         if count == 1:
-            return fn(domain, lit1, mode)
+            if has_pos:
+                return fn(domain, lit1, pos, mode)
+            else:
+                return fn(domain, lit1, mode)
         elif count == 2:
-            return fn(domain, lit1, lit2, mode)
+            if has_pos:
+                return fn(domain, lit1, lit2, pos, mode)
+            else:
+                return fn(domain, lit1, lit2, mode)
+        elif count == 3:
+            if has_pos:
+                return fn(domain, lit1, lit2, lit3, pos, mode)
+            else:
+                return fn(domain, lit1, lit2, lit3, mode)
         else:
             return "Unsupported number of literals."
     except Exception as e:
@@ -58,14 +68,24 @@ def prob_MAR1(domain, lit, mode):
     return f"P({lit})={prob:.4f}"
 
 
-def prob_MAR2(domain, lit1, lit2, mode):
+def prob_MAR2(domain, lit1, lit2, pos, mode):
     literal_to_tokens = get_literal_token_mapping(domain)
-    token1 = literal_to_tokens[lit1]
-    token2 = literal_to_tokens[lit2]
-    inputs = [token1, token2]
+    tok1 = literal_to_tokens[lit1]
+    tok2 = literal_to_tokens[lit2]
+    inputs = ([tok1, tok2], pos)
     query_type = "MAR2"
     prob = compute_probability(domain, query_type, inputs, mode)
-    return f"P({lit})={prob:.4f}"
+    return f"P([{lit1},{lit2}])={prob:.4f} at position {pos}"
+
+
+# def prob_MAR3(domain, lit1, lit2, mode):
+#     literal_to_tokens = get_literal_token_mapping(domain)
+#     token1 = literal_to_tokens[lit1]
+#     token2 = literal_to_tokens[lit2]
+#     inputs = [token1, token2]
+#     query_type = "MAR2"
+#     prob = compute_probability(domain, query_type, inputs, mode)
+#     return f"P({lit})={prob:.4f}"
 
 
 def prob_COND1(domain, lit1, lit2, mode):
@@ -78,30 +98,54 @@ def prob_COND1(domain, lit1, lit2, mode):
     return f"P({lit2}|{lit1})={prob:.4f}"
 
 
-def prob_COND2(domain, lit1, lit2, mode):
+def prob_COND2(domain, lit1, lit2, pos, mode):
     literal_to_tokens = get_literal_token_mapping(domain)
     token1 = literal_to_tokens[lit1]
     token2 = literal_to_tokens[lit2]
-    inputs = [token1, token2]
+    inputs = ([token1, token2], pos)
     query_type = "COND2"
     prob = compute_probability(domain, query_type, inputs, mode)
-    return f"P({lit2}|{lit1})={prob:.4f}"
+    return f"P({lit2}|{lit1})={prob:.4f} at position {pos}"
 
 
-def prob_MMAP1(domain, lit, mode):
+def prob_COND3(domain, lit1, lit2, lit, pos, mode):
     literal_to_tokens = get_literal_token_mapping(domain)
-    token = literal_to_tokens[lit]
-    inputs = [token]
-    query_type = "MMAP1"
+    token1 = literal_to_tokens[lit1]
+    token2 = literal_to_tokens[lit2]
+    tokens_list = [token1, token2]
+    print("token list", tokens_list)
+    tok = literal_to_tokens[lit]
+    inputs = (tokens_list, tok, pos)
+    print("inputs", inputs)
+    query_type = "COND3"
     prob = compute_probability(domain, query_type, inputs, mode)
-    return f"P({lit})={prob:.4f}"
+    return f"P({lit}|[{lit1},{lit2}])={prob:.4f} at position {pos}"
 
 
-def prob_MMAP2(domain, lit, mode):
+# def prob_MMAP1(domain, lit, mode):
+#     literal_to_tokens = get_literal_token_mapping(domain)
+#     token = literal_to_tokens[lit]
+#     inputs = [token]
+#     query_type = "MMAP1"
+#     prob = compute_probability(domain, query_type, inputs, mode)
+#     return f"P({lit})={prob:.4f}"
+
+
+def prob_MMAP(domain, lit, mode):
     literal_to_tokens = get_literal_token_mapping(domain)
     token = literal_to_tokens[lit]
     inputs = [token]
-    query_type = "MMAP2"
+    query_type = "MMAP"
+    likely_token = compute_probability(domain, query_type, inputs, mode)
+    return f"The most likely token to appear after {lit} is {likely_token}."
+
+
+def prob_EVI(domain, lit, mode):
+    # TODO : modify that
+    literal_to_tokens = get_literal_token_mapping(domain)
+    token = literal_to_tokens[lit]
+    inputs = [token]
+    query_type = "EVI"
     prob = compute_probability(domain, query_type, inputs, mode)
     return f"P({lit})={prob:.4f}"
 
@@ -115,18 +159,19 @@ def generate_inputs(domain, mode, num_inputs, sql_conn=None):
 
 questions_by_type = {
     "Marginal": ["Marginal_1", "Marginal_2"],
-    "Conditional": ["Conditional_1", "Conditional_2"],
-    "Marginal Map": ["MarginalMap_1"],
-    "Direct Evidence": ["DirectEvidence_1"],
+    "Conditional": ["Conditional_1", "Conditional_2", "Conditional_3"],
+    "Marginal Map": ["MarginalMap"],
+    "Direct Evidence": ["DirectEvidence"],
 }
 
 question_description = {
-    "Marginal_1": "P(Literal 1)",
-    "Marginal_2": "P(Literal 1, Literal 2)",
-    "Conditional_1": "P(Literal 2 | Literal 1), where Literal 2 appears anywhere after Literal 1",
-    "Conditional_2": "P(Literal 2 | Literal 1), where Literal 2 appears immediately after Literal 1",
-    "MarginalMap_1": "What is the most likely next literal after Literal 1?",
-    "DirectEvidence_1": "....",
+    "Marginal_1": "P(Literal 1) ?",
+    "Marginal_2": "P([Literal1,Literal2]), given that Literal 1 appears at position p ?",
+    "Conditional_1": "P(Literal 2 | Literal 1), where Literal 2 appears anywhere after Literal 1 ?",
+    "Conditional_2": "P(Literal 2 | Literal 1), where Literal 2 appears immediately after Literal 1 at position p ?",
+    "Conditional_3": "P(Literal 3 | [Literal 1,Literal 2]) given that the sequence appears at position p ?",
+    "MarginalMap": "What is the most likely token appearing anywhere after Literal 1 ?",
+    "DirectEvidence": "What is the probability of seeing the structure of this query ?",
 }
 
 literal_count_by_question = {
@@ -134,8 +179,19 @@ literal_count_by_question = {
     "Marginal_2": 2,
     "Conditional_1": 2,
     "Conditional_2": 2,
-    "MarginalMap_1": 1,
-    "DirectEvidence_1": 1,
+    "Conditional_3": 3,
+    "MarginalMap": 1,
+    "DirectEvidence": 1,
+}
+
+position_mapping = {
+    "Marginal_1": False,
+    "Marginal_2": True,
+    "Conditional_1": False,
+    "Conditional_2": True,
+    "Conditional_3": True,
+    "MarginalMap": False,
+    "DirectEvidence": False,
 }
 
 question_to_function = {
@@ -143,9 +199,12 @@ question_to_function = {
     "Marginal_2": prob_MAR2,
     "Conditional_1": prob_COND1,
     "Conditional_2": prob_COND2,
-    "MarginalMap_1": prob_MMAP1,
-    "DirectEvidence_1": prob_MMAP2,
+    "Conditional_3": prob_COND3,
+    "MarginalMap": prob_MMAP,
+    "DirectEvidence": prob_EVI,
 }
+
+max_length_mapping = {"SQL": 35, "B": 40, "REDIS": 10, "JANUS": 25}
 
 
 with gr.Blocks() as demo:
@@ -159,7 +218,7 @@ with gr.Blocks() as demo:
         gr.Markdown("")
 
         mode = gr.Radio(
-            ["generate", "no-generate"],
+            ["with-generate", "no-generate"],
             value="no-generate",
             label="Mode:",
             interactive=True,
@@ -189,6 +248,12 @@ with gr.Blocks() as demo:
             literal2 = gr.Dropdown(
                 choices=get_tokens("SQL"), label="Literal 2", visible=False
             )
+            literal3 = gr.Dropdown(
+                choices=get_tokens("SQL"), label="Literal 3", visible=False
+            )
+            position = gr.Slider(
+                0, maximum=34, value=0, step=1, label="Position", visible=False
+            )
 
         # Update function
         def update_question_choices(qtype):
@@ -196,7 +261,7 @@ with gr.Blocks() as demo:
                 choices=[
                     (question_description[q], q) for q in questions_by_type[qtype]
                 ],
-                value=None,
+                value=questions_by_type[qtype][0],
             )
 
         query_type.change(
@@ -204,15 +269,22 @@ with gr.Blocks() as demo:
         )
 
         # When question changes, update literals
-        def update_literal_dropdowns(q):
+        def update_literal_dropdowns(q, domain):
             count = literal_count_by_question.get(q, 1)
+            print("count", count)
+            has_pos = position_mapping.get(q, False)
+            max_length = max_length_mapping.get(domain, 10) - 2
             return (
-                gr.update(visible=True, interactive=(count == 2)),
-                gr.update(visible=(count == 2), interactive=(count == 2)),
+                gr.update(visible=(count >= 1), interactive=(count >= 1)),
+                gr.update(visible=(count >= 2), interactive=(count >= 2)),
+                gr.update(visible=(count >= 3), interactive=(count >= 3)),
+                gr.update(visible=has_pos, interactive=has_pos, maximum=max_length),
             )
 
         question.change(
-            fn=update_literal_dropdowns, inputs=question, outputs=[literal1, literal2]
+            fn=update_literal_dropdowns,
+            inputs=[question, domain],
+            outputs=[literal1, literal2, literal3, position],
         )
 
         prob_button = gr.Button("Get Probability")
@@ -223,18 +295,19 @@ with gr.Blocks() as demo:
             return (
                 gr.update(choices=tokens, value=None, interactive=True),
                 gr.update(choices=tokens, value=None, interactive=True),
+                gr.update(choices=tokens, value=None, interactive=True),
                 tokens,
             )
 
         domain.change(
             fn=update_token_dropdowns,
             inputs=domain,
-            outputs=[literal1, literal2, token_list],
+            outputs=[literal1, literal2, literal3, token_list],
         )
 
         prob_button.click(
             fn=dispatch_probability_function,
-            inputs=[domain, question, literal1, literal2, mode],
+            inputs=[domain, question, literal1, literal2, literal3, position, mode],
             outputs=prob_output,
         )
 
@@ -243,7 +316,7 @@ with gr.Blocks() as demo:
             label="PostgreSQL Connection String",
             placeholder="dbname=? user=? password=? host=? port=?",
         )
-        num_inputs = gr.Slider(10, 10000, value=1000, step=10, label="Number of Inputs")
+        num_inputs = gr.Slider(10, 1000, value=500, step=10, label="Number of Inputs")
         generate_btn = gr.Button("Generate")
         output_file = gr.File()
         generation_status = gr.Label()
@@ -253,15 +326,14 @@ with gr.Blocks() as demo:
 
         domain.change(fn=toggle_input_fields, inputs=domain, outputs=sql_conn)
 
-        def run_generation(domain, sql_conn, num_inputs):
-            mode = "no-generate"
+        def run_generation(domain, mode, sql_conn, num_inputs):
             generation_status = f"Generating {num_inputs} inputs for {domain}..."
             zip_file = generate_inputs(domain, mode, int(num_inputs), sql_conn)
             return zip_file, f"Done! Download the generated inputs."
 
         generate_btn.click(
             fn=run_generation,
-            inputs=[domain, sql_conn, num_inputs],
+            inputs=[domain, mode, sql_conn, num_inputs],
             outputs=[output_file, generation_status],
         )
 if __name__ == "__main__":
--- inference.py
+++ inference.py
@@ -1,4 +1,12 @@
-from cfg2pc.query import marginal_query, marginal_query_cond, evi_query, marginal_sequence_query, marginal_query_contiguous_cond, sequential_query_contiguous_cond, marginal_map_query
+from cfg2pc.query import (
+    marginal_query,
+    marginal_query_cond,
+    evi_query,
+    marginal_sequence_query,
+    marginal_query_contiguous_cond,
+    sequential_query_contiguous_cond,
+    marginal_map_query,
+)
 from main import MODEL_DIR, get_literal_token_mapping, load_domains_config
 import torch
 from pathlib import Path
@@ -18,7 +26,7 @@ def load_model_and_info(domain: str, mode: str):
 
 
 def ask_query(
-    model, lit_map: dict, lit_size: int, max_length: int, query_type: str, inputs: list
+    model, lit_map: dict, lit_size: int, max_length: int, query_type: str, inputs: tuple
 ):
     match query_type:
         case "MAR1":
@@ -26,19 +34,21 @@ def ask_query(
             prob = abs(marginal_query(model, tok, lit_map, lit_size, max_length))
             return prob
         case "MAR2":
-            tok = inputs
-            prob = abs(marginal_sequence_query(model, tok, lit_map, lit_size, max_length))
+            """represent input as ([token1,...],pos)"""
+            sequence = inputs[0]
+            pos = inputs[1]
+            prob = abs(marginal_sequence_query(model, sequence, lit_map, lit_size, pos))
             return prob
         case "COND1":
-            """ represent input as ([token1, token1]) """
+            """represent input as ([token1, token1])"""
             tok = inputs[0]
             tok2 = inputs[1]
             prob = abs(
                 marginal_query_cond(model, tok, tok2, lit_map, lit_size, max_length)
             )
             return prob
-        case "COND2": 
-            """ represent input as ([token1, token1], pos) """
+        case "COND2":
+            """represent input as ([token1, token1], pos)"""
             tok = inputs[0][0]
             tok2 = inputs[0][1]
             pos = inputs[1]
@@ -47,26 +57,25 @@ def ask_query(
             )
             return prob
         case "COND3":
-            """ represent input as ([token1, token1, ...], pos) """
-            sequence = inputs[0][1:len(inputs)-1]
-            tok = inputs[0][len(inputs)-1]
-            pos = inputs[1]
+            """represent input as ([token1, token2...],tok, pos)"""
+            sequence, tok, pos = inputs
+            print(sequence, tok, pos)
             prob = abs(
-                sequential_query_contiguous_cond(model, tok, sequence, lit_map, lit_size, pos)
+                sequential_query_contiguous_cond(
+                    model, tok, sequence, lit_map, lit_size, pos
+                )
             )
             return prob
         case "EVI":
             seq = inputs
-            prob = abs(
-                evi_query(model, seq, lit_map, lit_size)
-            )
-            pass
+            prob = abs(evi_query(model, seq, lit_map, lit_size))
+            return prob
         case "MMAP":
             tok = inputs[0]
-            prob = abs(
-                marginal_query_cond(model, tok, lit_map, lit_size, max_length)
+            most_probable_token = marginal_map_query(
+                model, tok, lit_map, lit_size, max_length
             )
-            return prob
+            return most_probable_token
 
 
 if __name__ == "__main__":
