a8eaa5e87526a01aef1894c4b38da6d085496457
Annaelle <annaellebaiget@g.ucla.edu>
2025-06-06 13:41:37 -0700

    set up XML domain

--- main.py
+++ main.py
@@ -328,10 +328,10 @@ if __name__ == "__main__":
     config_file_path = "domains_config.json"
     domains_config = load_domains_config(config_file_path)
 
-    domain = "JANUS"
+    domain = "XML"
     domain_config = domains_config[domain]
 
-    num_inputs = 10000
+    num_inputs = 10
 
     build_model(
         domain,
